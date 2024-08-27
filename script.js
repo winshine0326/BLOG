1608cd5db1be2a69e52022f08e97641970a927c5
winshine0326 <shane10340@gmail.com>
2024-08-28 02:11:01 +0900

    test

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("hello world");
+const express = require('express')
+const app = express()
+var cors = require('cors')
+
+app.use(cors())
+
+app.get('/', function (req, res) {
+    res.send('Hello World')
+})
+app.get('/sound/:name', function (req, res) {
+    const { name } = req.params
+
+    if(name == "dog")
+        res.json({'sound' : '멍멍'})
+    else if(name == "cat")
+        res.json({'sound' : '야옹'})
+})
+
+app.listen(3000)
