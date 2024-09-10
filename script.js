cac3e9215230bb170a43ab9e306f82b188f20c13
winshine0326 <shane10340@gmail.com>
2024-09-10 14:23:52 +0900

    feat: 글 로드 기능 구현

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.getElementById('submitBtn').addEventListener('click', async function() { // 글 저장 버튼 클릭 시 실행
+document.getElementById('submitBtn').addEventListener('click', async function () { // 글 저장 버튼 클릭 시 실행
     const title = document.getElementById('title').value;
     const content = document.getElementById('content').value;
 
@@ -34,3 +34,41 @@ document.getElementById('submitBtn').addEventListener('click', async function() 
         alert('글 작성에 실패했습니다.');
     }
 });
+
+async function loadPosts() {
+    try {
+        const response = await fetch('https://localhost:3000/get');
+        if (response.ok) {
+            const posts = await response.json();
+
+            const contentBox = docuement.getElementById('contentBox');
+            contentBox.innerHTML = '';
+
+            if (posts.length === 0) {
+                contentBox.innerHTML = '<p>작성된 글이 없습니다.</p>';
+            }
+            else {
+                posts.forEach(post => {
+                    const postDiv = document.createElement('div');
+                    postDiv.classList.add('post');
+                    postDiv.innerHTML = `
+                                <h2>${post.title}</h2>
+                                <p>${post.content}</p>
+                            `;
+                    contentBox.appendChild(postDiv);
+                });
+            }
+        }
+        else{
+            alert('글을 불러오는 데 실패했습니다.');
+        }
+    }
+    catch(error){
+        console.error('Error:',error);
+        alert('서버와의 통신에 문제가 발생했습니다.');
+    }
+}
+
+
+
+
